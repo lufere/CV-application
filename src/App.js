a8029f6853db2e11933bc50a2fe15302b9cebd35
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-10-05 22:11:44 -0700

    Added the submit and edit functionality for the General section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import General from './components/General'
-import Education from './components/Education'
-import Experience from './components/Experience'
+// import Education from './components/Education'
+// import Experience from './components/Experience'
 
-function App() {
-  return (
-    <div className="App">
-      <General/>
-      <Education/>
-      <Experience/>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      phone: "",
+      editGeneral: true,
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event){
+    const target = event.target;
+    const name = target.name;
+    const value = target.type === 'checkbox'? target.checked : target.value;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleSubmit(event){
+    this.setState({
+      editGeneral: !this.state.editGeneral,
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <General
+          onChange = {this.handleChange}
+          editing = {this.state.editGeneral}
+          name = {this.state.name}
+          email = {this.state.email}
+          phone = {this.state.phone}
+          submit = {this.handleSubmit}
+        />
+        {/* <Education/>
+        <Experience/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/General.js
+++ src/components/General.js
@@ -1,21 +1,86 @@
 import React, { Component } from "react";
 
 class General extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+          name: "",
+          email: "",
+          phone: "",
+          editGeneral: false,
+        }
+    
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        this.props.onChange(event);
+    }
+
     render(){
-        return(
-            <section>
-                <form>
-                    <h2>General Information</h2>
-                    <label for = "name">Name</label>
-                    <input type = "text" id = "name" name = "name"></input>
-                    <label for = "email">E-mail</label>
-                    <input type = "email" id = "email" name = "email"></input>
-                    <label for = "phone">Phone number</label>
-                    <input type = "tel" id = "phone" name = "phone"></input>
-                    <input type = "button" value = "SUBMIT"></input>
-                </form>
-            </section>
-        );
+        if (this.props.editing) {            
+            return(
+                <section>
+                    <form>
+                        <h2>General Information: </h2>
+                        <label>Name
+                            <input 
+                                type = "text" 
+                                name = "name"
+                                value = {this.props.name}
+                                onChange = {this.handleChange}
+                            />
+                        </label>
+                        <label>E-mail: 
+                            <input 
+                                type = "email" 
+                                name = "email"
+                                value = {this.props.email}
+                                onChange = {this.handleChange}
+                            />
+                        </label>
+                        <label>Phone number: 
+                            <input 
+                                type = "tel" 
+                                name = "phone"
+                                value = {this.props.phone}
+                                onChange = {this.handleChange}
+                            />
+                        </label>
+                        <input
+                            type = "button"
+                            value = "SUBMIT"
+                            onClick = {this.props.submit}
+                        />
+                    </form>
+                </section>
+            );
+        }else{
+            return(
+                <section>
+                    <div>
+                        <h2>General Information: </h2>
+                        <h3>Name</h3>
+                            <p>
+                                {this.props.name}                            
+                            </p>
+                        <h3>Email</h3>
+                            <p>
+                                {this.props.email}
+                            </p>
+                        <h3>Phone</h3>
+                            <p>
+                                {this.props.phone}
+                            </p>
+                        <input
+                            type = "button"
+                            value = "EDIT"
+                            onClick = {this.props.submit}
+                        />
+                    </div>
+                </section>
+            );
+        }
     }
 }
 
