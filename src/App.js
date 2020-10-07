101e21be488becd763ed6f739126556ebe0ec63d
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-10-07 16:37:22 -0700

    Added edit and delete functionality for education entries

--- src/App.js
+++ src/App.js
@@ -16,11 +16,15 @@ class App extends Component{
       studyStart: "",
       studyEnd: "",
       education: [{name:"ITH", title: "Mechatronics", start: "2013-07-08", end: "2017-12-15"}],
+      educationIndex: null,
     }
 
     this.handleChange = this.handleChange.bind(this);
     this.handleGeneralSubmit = this.handleGeneralSubmit.bind(this);
     this.handleEducationSubmit = this.handleEducationSubmit.bind(this);
+    this.educationEdit = this.educationEdit.bind(this);
+    this.educationEditSubmit = this.educationEditSubmit.bind(this);
+    this.educationDelete = this.educationDelete.bind(this);
   }
 
   handleChange(event){
@@ -48,7 +52,53 @@ class App extends Component{
         start: this.state.studyStart,
         end: this.state.studyEnd,
       }]),
-    },()=>console.log(this.state.education));
+      schoolName: "",
+      studyTitle:"",
+      studyStart:"",
+      studyEnd:"",
+    });
+  }
+
+  educationEdit(event){
+    let index = parseInt(event.target.parentElement.id);
+    let target = this.state.education[index];
+    this.setState({
+      educationIndex: index,
+      schoolName: target.name,
+      studyTitle: target.title,
+      studyStart: target.start,
+      studyEnd: target.end,
+    });
+  }
+
+  educationEditSubmit(event){
+    let index = parseInt(event.target.parentElement.id);
+    const newEducation = this.state.education;
+    const editedSchool = {
+      name: this.state.schoolName,
+      title: this.state.studyTitle,
+      start: this.state.studyStart,
+      end: this.state.studyEnd,
+    }
+    newEducation.splice(index,1,editedSchool)
+    // console.log(newEducation);
+    this.setState({
+      education: newEducation,
+      schoolName: "",
+      studyTitle:"",
+      studyStart:"",
+      studyEnd:"",
+      educationIndex: null,
+    });
+  }
+
+  educationDelete(event){
+    let index = parseInt(event.target.parentElement.id);
+    const newEducation = this.state.education;
+    newEducation.splice(index,1);
+    this.setState({
+      education: newEducation,
+    });
   }
 
   render(){
@@ -64,12 +114,16 @@ class App extends Component{
         />
         <Education
           onChange = {this.handleChange}
-          schoolName = {this.schoolName}
-          studyTitle = {this.studyTitle}
-          studyStart = {this.studyStart}
-          studyEnd = {this.studyEnd}
+          schoolName = {this.state.schoolName}
+          studyTitle = {this.state.studyTitle}
+          studyStart = {this.state.studyStart}
+          studyEnd = {this.state.studyEnd}
           education = {this.state.education}
           submit = {this.handleEducationSubmit}
+          edit = {this.educationEdit}
+          editIndex = {this.state.educationIndex}
+          editSubmit = {this.educationEditSubmit}
+          delete = {this.educationDelete}
         />
         {/* <Experience/> */}
       </div>
--- src/components/Education.js
+++ src/components/Education.js
@@ -11,23 +11,14 @@ class Education extends Component{
     }
 
     render(){
-        let educationList = this.props.education.map((school)=>{
-            return(
-                <li key = {school.name+school.title}>
-                    {`Institution: ${school.name}, 
-                    Title: ${school.title}, 
-                    Start Date: ${school.start}
-                    End Date: ${school.end} `}
-                </li>
-                // <li>{school.name+", "+school.title}</li>
-            );
-        })
-        return(
-            <section>
-                <form>
-                    <h2>Education</h2>
+        let educationList = this.props.education.map((school, i)=>{
+		// console.log(this.props.editIndex);
+		// console.log(i);
+			if(this.props.editIndex === i){
+				return(
+					<div key = {school.name+school.title} id = {i}>
                         <label>School Name: 
-                            <input 
+                            <input
                                 type = "text" 
                                 name = "schoolName"
                                 value = {this.props.schoolName}
@@ -35,7 +26,7 @@ class Education extends Component{
                             />
                         </label>
                         <label>Title: 
-                            <input 
+                            <input
                                 type = "text" 
                                 name = "studyTitle"
                                 value = {this.props.studyTitle}
@@ -43,7 +34,67 @@ class Education extends Component{
                             />
                         </label>
                         <label>Start Date: 
+                            <input
+                                type = "date" 
+                                name = "studyStart"
+                                value = {this.props.studyStart}
+                                onChange = {this.handleChange}
+                            />
+                        </label>
+                        <label>End Date: 
                             <input 
+                                type = "date" 
+                                name = "studyEnd"
+                                value = {this.props.studyEnd}
+                                onChange = {this.handleChange}
+                            />
+                        </label>
+                        <input
+                            type = "button"
+                            value = "SUBMIT"
+                            onClick = {this.props.editSubmit}
+                        />
+                </div>
+				);
+			}else{
+				return(
+					<li key = {school.name+school.title} id = {i}>
+						<p>
+							{`Institution: ${school.name}, 
+							Title: ${school.title}, 
+							Start Date: ${school.start}
+							End Date: ${school.end} `}
+						</p>
+						<button onClick = {this.props.edit}>EDIT</button>
+						<button onClick = {this.props.delete}>DELETE</button>
+					</li>
+					// <li>{school.name+", "+school.title}</li>
+				);
+			}
+		})
+        if(this.props.editIndex === null){
+        return(
+            <section>
+                <form>
+                    <h2>Education</h2>
+                        <label>School Name: 
+                            <input
+                                type = "text" 
+                                name = "schoolName"
+                                value = {this.props.schoolName}
+                                onChange = {this.handleChange}
+                            />
+                        </label>
+                        <label>Title: 
+                            <input
+                                type = "text" 
+                                name = "studyTitle"
+                                value = {this.props.studyTitle}
+                                onChange = {this.handleChange}
+                            />
+                        </label>
+                        <label>Start Date: 
+                            <input
                                 type = "date" 
                                 name = "studyStart"
                                 value = {this.props.studyStart}
@@ -68,7 +119,19 @@ class Education extends Component{
                     {educationList}
                 </ul>
             </section>
+		);
+	}else{ 
+        return(
+            <section>
+                <form>
+                    <h2>Education</h2>
+                    <ul>
+                        {educationList}
+                    </ul>
+                </form>
+            </section>
         );
+    }
     }
 }
 
