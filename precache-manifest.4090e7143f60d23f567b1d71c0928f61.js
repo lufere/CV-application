4028ce2242288345431f634b9133d277124f6dc8
Luis Rendon <luisfelipe.rendon@gmail.com>
2021-01-01 13:22:25 -0700

    Updates

---
+++ precache-manifest.4090e7143f60d23f567b1d71c0928f61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9d01e2c39a81ea3de4b5bfe9c5ddd15",
+    "url": "/CV-application/index.html"
+  },
+  {
+    "revision": "743b51f64c184992217f",
+    "url": "/CV-application/static/css/main.d641c269.chunk.css"
+  },
+  {
+    "revision": "dcac96160a6c73b2fd21",
+    "url": "/CV-application/static/js/2.78ddb5e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CV-application/static/js/2.78ddb5e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "743b51f64c184992217f",
+    "url": "/CV-application/static/js/main.a9ae541b.chunk.js"
+  },
+  {
+    "revision": "64d9fe649640e7ef8b87",
+    "url": "/CV-application/static/js/runtime-main.9ca1b4fc.js"
+  }
+]);
